b8de18e6312fb4c15693b59ea8558d88935268e5
Armani <armanivvv@gmail.com>
2018-11-16 14:20:17 +0800

    added validations and permissions to cocktail object

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_061910) do
+ActiveRecord::Schema.define(version: 2018_11_16_055910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 2018_11_15_061910) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
+    t.string "description"
+    t.string "history"
+    t.string "photo"
   end
 
   create_table "doses", force: :cascade do |t|
